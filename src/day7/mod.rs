6df5dfae75a079be84815c3d3390affaad79e9a1
schwiet <seth.schwiet@pm.me>
2023-12-08 17:32:58 -0800

    added hand classification for day 7 puzzle

--- src/day7/mod.rs
+++ src/day7/mod.rs
@@ -2,24 +2,25 @@ use super::util::open_file_as_bufreader;
 use std::collections::HashMap;
 use std::io::{self, BufRead};
 
-const CARDS: [(&str, i32); 13] = [
-    ("A", 14),
-    ("K", 13),
-    ("Q", 12),
-    ("J", 11),
-    ("T", 10),
-    ("9", 9),
-    ("8", 8),
-    ("7", 7),
-    ("6", 6),
-    ("5", 5),
-    ("4", 4),
-    ("3", 3),
-    ("2", 2),
+const CARDS: [(char, u8); 13] = [
+    ('A', 12),
+    ('K', 11),
+    ('Q', 10),
+    ('J', 9),
+    ('T', 8),
+    ('9', 7),
+    ('8', 6),
+    ('7', 5),
+    ('6', 4),
+    ('5', 3),
+    ('4', 2),
+    ('3', 1),
+    ('2', 0),
 ];
 
 #[derive(Debug, Eq, PartialEq)]
 enum PokerHand {
+    Undefined,
     HighCard,
     OnePair,
     TwoPair,
@@ -30,6 +31,44 @@ enum PokerHand {
     FourOfAKind,
     StraightFlush,
     FiveOfAKind,
+}
+
+#[derive(Debug)]
+struct Hand {
+    hand_type: PokerHand,
+    cards: [u8; 5],
+    str: String,
+}
+
+fn classify_hand(hand: &str, card_values: &HashMap<char,u8>) -> PokerHand {
+    let mut card_counts: HashMap<char, u8> = HashMap::new();
+    let mut i = 0;
+    let mut cards: [u8; 5] = [0; 5];
+    for char in hand.chars() {
+        if let Some(val) = card_values.get(char) {
+            cards[i] = *val;
+        }
+        let count = card_counts.entry(char).or_insert(0);
+
+        *count += 1;
+        i += 1;
+    }
+    let mut count_freq = [0;5];
+
+    // Iterate over the values
+    for count in card_counts.values() {
+        count_freq[(count - 1) as usize] += 1
+    }
+
+    match count_freq {
+        [5, 0, 0, 0, 0] => PokerHand::HighCard,
+        [1, 2, 0, 0, 0] => PokerHand::TwoPair,
+        [2, 0, 1, 0, 0] => PokerHand::ThreeOfAKind,
+        [0, 1, 1, 0, 0] => PokerHand::FullHouse,
+        [1, 0, 0, 1, 0] => PokerHand::FourOfAKind,
+        [0, 0, 0, 0, 1] => PokerHand::FiveOfAKind,
+        _ => PokerHand::Undefined,
+    }
 }
 
 pub fn run() -> io::Result<()> {
@@ -46,7 +85,8 @@ pub fn run() -> io::Result<()> {
         // Splitting the line on whitespace
         let tokens: Vec<&str> = line.split_whitespace().collect();
 
-        println!("{}, {}", tokens[0], tokens[1]);
+        let poker_hand = classify_hand(tokens[0], &map);
+        println!("{:?}, {}, {}", poker_hand, tokens[0], tokens[1]);
     }
 
     Ok(())
