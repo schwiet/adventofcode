f6d20ac18196263a1bd7ffb689b6ec18f5171b14
schwiet <seth.schwiet@pm.me>
2023-12-13 15:04:12 -0800

    generalized day 12 solution to work for both puzzles by keeping track of previously-found permutations

--- src/day12/mod.rs
+++ src/day12/mod.rs
@@ -1,7 +1,11 @@
 use super::util::open_file_as_bufreader;
+use std::collections::HashMap;
 use std::io::{self, BufRead};
 
-fn perm_count(seq: &[u32], group: &[char]) -> u64 {
+type KnownPaths = HashMap<String, u64>;
+type VisitedGroup = HashMap<String, KnownPaths>;
+
+fn perm_count(mut visited: &mut VisitedGroup, seq: &[u32], group: &[char]) -> u64 {
     if seq.len() == 0 {
         return 1;
     }
@@ -14,40 +18,49 @@ fn perm_count(seq: &[u32], group: &[char]) -> u64 {
     // the sequence
     let min_space = (seq.iter().fold(0, |acc, s| acc + s + 1) - 1) as usize;
 
+    let seq_key: String = seq.iter().map(|&n| n.to_string()).collect();
+    let key: String = group.iter().collect();
+    if let Some(knowns) = visited.get(&key) {
+        if let Some(k) = knowns.get(&seq_key) {
+            // println!("already known:\n\t{key}\n\t{seq_key}\n\tcount: {k}");
+            return *k;
+        }
+    }
+
     let mut ct: u64 = 0;
     let mut group = &group[..];
     let s = seq[0] as usize;
     while group.len() >= min_space {
-        print!("- Checking {:?} {:?}", group, seq);
+        let key: String = group.iter().collect();
+
         if group.len() == s || group[s] == '?' || group[s] == '.' {
             // can't satisfy seq if any dots
             if !group[..s].contains(&'.') {
                 // print!(" - NO DOTS {:?}", group[..s].to_vec());
                 // if there is more of the sequence to try to fit in the group
                 if seq.len() > 1 {
-                    print!("\n");
+                    // print!("\n");
                     // if there are more characters
                     if group.len() > s {
-                        println!("  - took {:?}", group[..s + 1].to_vec());
-                        ct += perm_count(&seq[1..], &group[s + 1..])
+                        ct += perm_count(visited, &seq[1..], &group[s + 1..]);
                     } else {
-                        print!(" x (not enough chars left)");
+                        // print!(" x (not enough chars left)");
                     }
                 } else if group[s..].contains(&'#') {
                     // reached the end of the sequence, but there are hashes left
-                    print!(" x (has remaining #)");
+                    // print!(" x (has remaining #)");
                 } else {
                     // fit the whole sequence! +1
-                    print!(" √");
+                    // print!(" √");
                     ct += 1;
                 }
             } else {
-                print!(" x (has . in space)");
+                // print!(" x (has . in space)");
             }
         } else {
-            print!(" x (has # joining preventing)");
+            // print!(" x (has # joining preventing)");
         }
-        print!("\n");
+        // print!("\n");
 
         // This has to be part of the group, so there are no more permuations
         // of the current sequence beyond this point
@@ -57,24 +70,37 @@ fn perm_count(seq: &[u32], group: &[char]) -> u64 {
             break;
         }
     }
+
+    // println!("Inserting:\n\t{key}\n\t{seq_key}\n\tCount: {ct}");
+    visited
+        .entry(key)
+        .or_insert_with(|| HashMap::new())
+        .entry(seq_key)
+        .or_insert(ct);
     return ct;
 }
 
-fn get_groups(row: &str) -> Option<Vec<char>> {
-    Some(
-        row.split(".")
-            .filter(|s| s != &"")
-            .map(|s| s.chars().collect())
-            .collect::<Vec<Vec<char>>>()
-            .join(&'.'),
-    )
+fn get_groups(row: &str, num_copies: usize) -> Option<Vec<char>> {
+    let orig = row.chars().collect::<Vec<char>>();
+
+    let copies: Vec<Vec<char>> = (0..num_copies).map(|_| orig.to_vec()).collect();
+
+    // println!("COPIES {:?}", copies);
+    // println!("JOINED {:?}", copies.join(&'?'));
+
+    Some(copies.join(&'?'))
 }
 
-fn get_sequence(seq: &str) -> Option<Vec<u32>> {
-    Some(seq.split(",").map(|s| s.parse::<u32>().unwrap()).collect())
+fn get_sequence(seq: &str, num_copies: usize) -> Option<Vec<u32>> {
+    let seq: Vec<u32> = seq.split(",").map(|s| s.parse::<u32>().unwrap()).collect();
+
+    let result: Vec<Vec<u32>> = (0..num_copies).map(|_| seq.to_vec()).collect();
+
+    Some(result.concat())
 }
 
 pub fn run() -> io::Result<()> {
+    let copies: usize = 1;
     // Create a new BufReader for the file
     let reader = open_file_as_bufreader("src/day12/input.txt")?;
 
@@ -84,14 +110,16 @@ pub fn run() -> io::Result<()> {
 
         let mut iter = line.split_whitespace();
         let (r, s) = (
-            iter.next().and_then(get_groups),
-            iter.next().and_then(get_sequence),
+            iter.next().and_then(|row| get_groups(row, copies)),
+            iter.next().and_then(|seq| get_sequence(seq, copies)),
         );
+
+        let mut visited: &mut VisitedGroup = &mut HashMap::new();
 
         match (r, s) {
             (Some(row), Some(seq)) => {
                 println!("           {:?} {:?}", row, seq);
-                let count = perm_count(&seq, &row);
+                let count = perm_count(&mut visited, &seq, &row);
                 sum += count;
                 println!("Count: {count}\n---------------------------------\n");
             }
