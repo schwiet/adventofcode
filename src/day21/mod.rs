5217a480f0151969e812fe8a2a0dc80db205ae76
schwiet <seth.schwiet@pm.me>
2023-12-26 16:31:42 -0800

    solution for first puzzle of say 21

--- src/day21/mod.rs
+++ src/day21/mod.rs
@@ -2,77 +2,70 @@ use super::util::open_file_as_bufreader;
 use std::collections::HashSet;
 use std::io::{self, BufRead};
 
-fn reachable_gardens(
-    max_step: u64,
-    step: u64,
-    area: &HashSet<(u64, u64)>,
-    garden: &(u64, u64),
-) -> u64 {
-    if area.get(garden) == None || step > max_step {
-        return 0;
-    }
+fn run_game(gardens: &HashSet<(u64, u64)>, gen: &HashSet<(u64, u64)>) -> HashSet<(u64, u64)> {
+    let mut next_gen: HashSet<(u64, u64)> = HashSet::new();
 
-    let mut sum = 0u64;
+    for g in gen {
+        // check possible gardens
+        // up
+        if g.0 > 0 && gardens.get(&(g.0 - 1, g.1)) != None {
+            next_gen.insert((g.0 - 1, g.1));
+        }
 
-    // LEFT
-    if garden.1 > 0 {
-        let l = (garden.0, garden.1 - 1);
-        if let Some(g) = area.get(&l) {
-            println!("Garden at {:?}", l);
-            if step >= max_step {
-                sum += 1;
-            } else {
-                sum += reachable_gardens(max_step, step + 1, area, &l);
-            }
+        // left
+        if g.1 > 0 && gardens.get(&(g.0, g.1 - 1)) != None {
+            next_gen.insert((g.0, g.1 - 1));
+        }
+
+        // right
+        if gardens.get(&(g.0, g.1 + 1)) != None {
+            next_gen.insert((g.0, g.1 + 1));
+        }
+
+        // down
+        if gardens.get(&(g.0 + 1, g.1)) != None {
+            next_gen.insert((g.0 + 1, g.1));
+        }
+
+        // check current state
+        let mut has_neighbors: bool = false;
+        // up
+        if g.0 > 0 && gen.get(&(g.0 - 1, g.1)) != None {
+            has_neighbors = true;
+        }
+
+        // left
+        if g.1 > 0 && gen.get(&(g.0, g.1 - 1)) != None {
+            has_neighbors = true;
+        }
+
+        // right
+        if gen.get(&(g.0, g.1 + 1)) != None {
+            has_neighbors = true;
+        }
+
+        // down
+        if gen.get(&(g.0 + 1, g.1)) != None {
+            has_neighbors = true;
+        }
+
+        if has_neighbors {
+            next_gen.insert(*g);
         }
     }
 
-    // UP
-    if garden.0 > 0 {
-        let u = (garden.0 - 1, garden.1);
-        if let Some(g) = area.get(&u) {
-            println!("Garden at {:?}", u);
-            if step >= max_step {
-                sum += 1;
-            } else {
-                sum += reachable_gardens(max_step, step + 1, area, &u);
-            }
-        }
-    }
-
-    // RIGHT
-    let r = (garden.0, garden.1 + 1);
-    if let Some(g) = area.get(&r) {
-        println!("Garden at {:?}", r);
-        if step >= max_step {
-            sum += 1;
-        } else {
-            sum += reachable_gardens(max_step, step + 1, area, &r);
-        }
-    }
-
-    // DOWN
-    let d = (garden.0 + 1, garden.1);
-    if let Some(g) = area.get(&d) {
-        println!("Garden at {:?}", d);
-        if step >= max_step {
-            sum += 1;
-        } else {
-            sum += reachable_gardens(max_step, step + 1, area, &d);
-        }
-    }
-
-    return sum;
+    return next_gen;
 }
 
 pub fn run() -> io::Result<()> {
     // Create a new BufReader for the file
-    let reader = open_file_as_bufreader("src/day21/example.txt")?;
+    let reader = open_file_as_bufreader("src/day21/input.txt")?;
 
     let mut i = 0u64;
     let mut s = (0u64, 0u64);
 
     let mut gardens: HashSet<(u64, u64)> = HashSet::new();
+    let mut gen: HashSet<(u64, u64)> = HashSet::new();
 
     for line in reader.lines() {
         let line = line?;
@@ -84,6 +77,7 @@ pub fn run() -> io::Result<()> {
                 }
                 'S' => {
                     gardens.insert((i, j as u64));
+                    gen.insert((i, j as u64));
                     s = (i, j as u64);
                 }
                 _ => { /* ignore others */ }
@@ -93,9 +87,14 @@ pub fn run() -> io::Result<()> {
         i += 1;
     }
 
-    let sum = reachable_gardens(1, 0, &gardens, &s);
+    for i in 0..64 {
+        gen = run_game(&gardens, &gen)
+    }
 
-    println!("Reachable Gardens: {sum}, {:?}", s);
+    println!("Starting Garden: {:?}", s);
+    println!("Visited Gardens: {}", gen.len());
+    // 715 is too low
+    // 1663 is also too low
 
     Ok(())
 }
